01ce5e3f50d7db559ed60ab4578e23a7c5e1fa21
AKStudio09 <126498159+AKStudio09@users.noreply.github.com>
2023-03-31 10:27:30 +0000

    bug in the navbar fixed     modified: Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import { styles } from "../styles";
-import { NavLinks } from "../constants";
+import { navLinks } from "../constants";
 import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
