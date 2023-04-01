33644cd93b374b5d7c455a6215e8582a80a210f0
AKStudio09 <126498159+AKStudio09@users.noreply.github.com>
2023-04-01 15:55:16 +0000

    small changes 	modified:   package-lock.json 	modified:   package.json 	new file:   postcss.config.js 	modified:   src/components/Navbar.jsx 	modified:   vite.config.js

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,8 +11,15 @@ const Navbar = () => {
   return (
     <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary `}>
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
-        <Link to="/" className="flex items-center gap-2" onClick={() => { setActive(""); window.scrollTo(0, 0); }}>
+        <Link 
+          to="/" 
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive(""); 
+            window.scrollTo(0, 0);
+          }}>
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          <p className="text-white text-[18px] font-bold cursor-pointer">AKStudios <span className="sm:block hidden">| Portfolio</span></p>
         </Link>
       </div>
     </nav>
